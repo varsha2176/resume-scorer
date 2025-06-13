4dcf1e8b6021a1278daf47b55dfd0a7624f1a5c8
varsha2176 <aaruna.varshhh0760@gmail.com>
2025-06-13 11:01:03 +0530

    Added HTML frontend and templates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")

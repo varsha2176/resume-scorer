05521b1c865555e87db34d76eb9d64557b6ae25f
varsha2176 <aaruna.varshhh0760@gmail.com>
2025-06-13 11:06:11 +0530

    Fix deploy issue - correct templates

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
+async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/score-resume", response_class=HTMLResponse)
